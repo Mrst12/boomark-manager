fdf4d422ab17659ecc1bf69927d73dcbeb591c1c
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2023-05-19 10:14:18 +0100

    add the mobile hamburger button and menu along with the javascript to complete the menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
 
-//tabs menu event listener
-
+// Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
 
+// Hamburger button listener
+btn.addEventListener("click", navToggle);
+
 function onTabClick(e) {
-  //deactivate all tabs
+  // Deactivate all tabs
   tabs.forEach((tab) => {
     tab.children[0].classList.remove(
       "border-softRed",
@@ -15,14 +20,26 @@ function onTabClick(e) {
     );
   });
 
-  //hide all panels
+  // Hide all panels
   panels.forEach((panel) => panel.classList.add("hidden"));
 
-  //activate a new tab and panel based on the target
+  // Activate a new tab and panel based on the target
   e.target.classList.add("border-softRed", "border-b-4");
   const classString = e.target.getAttribute("data-target");
   document
     .getElementById("panels")
     .getElementsByClassName(classString)[0]
     .classList.remove("hidden");
+}
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
 }
